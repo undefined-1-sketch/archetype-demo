a88e1539301f54f708990bc5334d5fec11072574
baishijing <1126635345@qq.com>
2024-05-08 11:20:01 +0800

    init

---
+++ arbamboo-plugin-archetype/src/test/java/org/example/TestExampleServlet.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import junit.framework.TestCase;
+
+/**
+ * Testing {@link org.example.ExampleMacro}
+ */
+public class TestExampleServlet extends TestCase
+{
+    public void testBasic()
+    {
+        // add test here...
+    }
+}
